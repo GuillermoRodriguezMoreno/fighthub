25e6e73f5f3373bee11e76fd430dc783570912a4
guille <guille@ejemplo.es>
2025-06-01 20:04:27 +0200

    [MVP] redirect create event onSuccess

--- hooks/event/use-new-event-mutation.ts
+++ hooks/event/use-new-event-mutation.ts
@@ -1,16 +1,20 @@
 import { newEvent } from "@/clients/event-client";
+import { path } from "@/config/path";
 import { EventRequest } from "@/domains/event";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export function useNewEventMutation() {
     const queryClient = useQueryClient();
+    const router = useRouter();
     return useMutation({
         mutationKey: [`new-event`],
         mutationFn: async (newEventRequest: EventRequest) => {
             return newEvent(newEventRequest);
         },
-        onSuccess: () => {
+        onSuccess: (newEventId) => {
             queryClient.invalidateQueries({ queryKey: ["events"] });
+            router.push(`${path.dashboard.events.base}/${newEventId}`)
         },
     });
 }
