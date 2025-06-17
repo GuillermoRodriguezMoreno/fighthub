8f72174ced89565150f5b095f6fffa0b9eee22a8
guille <guille@ejemplo.es>
2025-06-17 03:03:23 +0200

    [MVP] lint

--- app/(protected)/dashboard/fights/page.tsx
+++ app/(protected)/dashboard/fights/page.tsx
@@ -2,5 +2,5 @@ import { path } from "@/config/path";
 import { redirect } from "next/navigation";
 
 export default function Page() {
-  redirect(path.dashboard.fights.popular);
+  redirect(path.dashboard.fights.all);
 }
