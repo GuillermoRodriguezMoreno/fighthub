0768ce5002fcda5ec5191e717240d819115efcba
guille <guille@ejemplo.es>
2025-06-17 02:38:21 +0200

    [MVP] export default page

--- app/(protected)/dashboard/clubs/[club_id]/page.tsx
+++ app/(protected)/dashboard/clubs/[club_id]/page.tsx
@@ -2,7 +2,7 @@ import { ClubPageContainer } from "@/components/clubs/pages/club-page";
 
 type Props = { params: { club_id: string } };
 
-export async function Page({ params }: Props) {
+export default async function Page({ params }: Props) {
   const { club_id } = await params;
   return <ClubPageContainer clubId={club_id} />;
 }
